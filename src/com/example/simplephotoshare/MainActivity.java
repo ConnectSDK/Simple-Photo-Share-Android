ee61cc53e30d7fe6dc5f69700c84430a6f3b7af1
Jeremy White <jeremy.white@idean.com>
2014-04-17 11:32:58 -0700

    Removed Fragment view for better simplicity, removed need for appcompat

--- src/com/example/simplephotoshare/MainActivity.java
+++ src/com/example/simplephotoshare/MainActivity.java
@@ -2,6 +2,16 @@ package com.example.simplephotoshare;
 
 import java.util.List;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+
+import com.connectsdk.device.ConnectableDevice;
 import com.connectsdk.device.ConnectableDeviceListener;
 import com.connectsdk.device.DevicePicker;
 import com.connectsdk.discovery.CapabilityFilter;
@@ -11,25 +21,8 @@ import com.connectsdk.service.DeviceService.PairingType;
 import com.connectsdk.service.capability.MediaPlayer;
 import com.connectsdk.service.capability.MediaPlayer.MediaLaunchObject;
 import com.connectsdk.service.command.ServiceCommandError;
-import com.connectsdk.device.ConnectableDevice;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.app.AlertDialog;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.os.Build;
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 	private DevicePicker mDevicePicker;
 	private ConnectableDevice mDevice;
 	
@@ -47,17 +40,7 @@ public class MainActivity extends ActionBarActivity {
 		
 		mDevicePicker = new DevicePicker(this);
 		
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
-	
-	protected void onResume() {
-		super.onResume();
-		
-		PlaceholderFragment fragment = (PlaceholderFragment) getSupportFragmentManager().getFragments().get(0);
-		Button imageButton = (Button) fragment.getView().findViewById(R.id.imageButton);
+		Button imageButton = (Button) findViewById(R.id.imageButton);
 		imageButton.setOnClickListener(showImageClickListener);
 	};
 	
@@ -135,42 +118,5 @@ public class MainActivity extends ActionBarActivity {
 			mDevice = null;
 		}
 	};
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
 
 }
